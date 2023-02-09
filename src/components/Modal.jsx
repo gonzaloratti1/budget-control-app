3e49645b1ac14b97b4a62ecbbab64c886999bf41
gonzaloratti1 <gonzaloratti1@gmail.com>
2023-02-08 23:21:24 -0300

    update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -87,11 +87,11 @@ const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto, gastoEdita
                         value={categoria}
                         onChange={e => setCategoria(e.target.value)}>
                         <option value="">-- Seleccione --</option>
-                        <option value="ahorro">Ahorro </option>
-                        <option value="comida">Comida </option>
-                        <option value="casa">Casa </option>
-                        <option value="gastos">Gastos Varios </option>
-                        <option value="ocio">Ocio </option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
                         <option value="salud">Salud</option>
                         <option value="suscripciones">Suscripciones</option>
                     </select>
